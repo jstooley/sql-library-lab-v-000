3728e9cf97b4016c163f29beaf5bbe8bcdb8db2d
Justin Tooley <jstooley823@gmail.com>
2017-09-27 18:02:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -9,7 +9,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species.value, species.count FROM species ORDER BY species.count DESC"
 end
 
 def select_name_and_series_subgenres_of_authors
