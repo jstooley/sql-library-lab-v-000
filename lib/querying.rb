6a463e0ab382b46cd86521e42d90e11187ee5e55
Justin Tooley <jstooley823@gmail.com>
2017-09-27 18:27:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,7 @@ def select_series_title_with_most_human_characters
    JOIN character_books ON book_id = books.id
    JOIN characters on character_books.character_id = characters.id
    WHERE characters.species = 'human' GROUP BY series.title
-   ORDER BY COUNT(*) DESC LIMIT 1""
+   ORDER BY COUNT(species) DESC LIMIT 1""
 end
 
 def select_character_names_and_number_of_books_they_are_in
